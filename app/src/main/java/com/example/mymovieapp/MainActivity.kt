c40d28a773d666b409f942496c73b414dd792b0c
Gregorio Massara <gregorio.massara1@bbc.co.uk>
2025-02-25 00:36:54 +0000

    Skeleton app with lazy list view of movies

---
+++ app/src/main/java/com/example/mymovieapp/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.example.mymovieapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mymovieapp.ui.theme.MyMovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MyMovieAppTheme {
+        Scaffold(
+            modifier = Modifier.padding(3.dp),
+            content = { content() },
+            containerColor = Color.Cyan,
+            topBar = {
+                TopAppBar(
+                    title = { Text("Movies") },
+                    colors = TopAppBarColors(
+                        containerColor = Color.Magenta,
+                        scrolledContainerColor = Color.Cyan,
+                        navigationIconContentColor = Color.Cyan,
+                        titleContentColor = Color.White,
+                        actionIconContentColor = Color.Cyan,
+                    )
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    containerColor = Color.Magenta,
+                    onClick = { Log.i("Hello", "click fab") }
+                ) { }
+            }
+        )
+    }
+}
+
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Space Odyssey",
+        "Blade Runner",
+        "Mad Max",
+        "Twin Peaks",
+        "Mullholland Drive",
+        "Stalker",
+        "Amarcord",
+        "Deserto Rosso"
+    )
+) {
+    Column(modifier = Modifier.padding(top = 76.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(it, onItemClick = { movie ->
+                    Log.d("TAG", movie)
+                })
+            }
+
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String, onItemClick: (String) -> Unit = {}) {
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .clickable { onItemClick(movie) }
+        .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier
+                .padding(24.dp)
+                .size(100.dp),
+                shape = RectangleShape) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "")
+
+            }
+            Text(movie)
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApp {
+        MainContent()
+    }
+}
